bbf898c90537075ac5d795ea4664df26f3ff4e73
vikranthposa <vikranth.iitk@gmail.com>
2017-07-05 16:57:15 +0530

    Update FindEndianness.c
    The answer the opposite ie., wrong in the previous commit, which has been rectified now

--- FindEndianness.c
+++ FindEndianness.c
@@ -1,16 +1,21 @@
+/*
+ Big-endian is an order in which the "big end" (most significant value in the sequence) is stored first (at the lowest storage address)
+ For Networking ---- Standard byte order for networks is big endian, also known as network byte order.
+*/
+
 #include <stdio.h>
 int main()
 {
-    unsigned int i = 1;
+    unsigned int i = 0x10FE;
     char *c = (char *) &i;
     // caution : Character pointer should be taken so that it will pointer to the starting byte of interger
     // if we take directly value then it will be typecasted always to value 1 in this case
     //  char c = (char)i;   Here value of  "c" will be alwasy one
     
-    if (*c)
-        printf("B endian");
+    if (*c == 0xFF)
+        printf("Little endian");
     else
-        printf("L endian");
+        printf("Big endian");
    
     return 0;
 }
